0a720b3aae479ed3f12a9bd842758091066b884f
Darren Royle <darren@ventureharbour.com>
2022-01-14 15:03:37 +0000

    More roles

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   careers: {
     Information: ["intro", "scoring"],
-    Engineering: ["eng/eng1", "eng/eng2"],
+    Engineers: ["eng/eng1", "eng/eng2", "eng/eng3"],
+    "Engineering Manager": ["eng-manager/m1"],
   },
 };
