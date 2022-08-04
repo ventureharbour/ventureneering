d28e221a207e451454c91c14d0cbe87a755407ff
Andrew Hudson <andrew@ventureharbour.com>
2022-08-04 14:18:36 +0100

    further continuation on the blog post

--- blog/2022-08-04-container-queries/Block.jsx
+++ blog/2022-08-04-container-queries/Block.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Block = () => {
   return (
-    <div class="block-wrapper">
+    <div class="block-container">
       <article class="block">
         <header>
           <img
